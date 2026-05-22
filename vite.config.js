ecec0589fd869ea1c1fade25b993375918b16138
byungchan3077 <byungchan3077@naver.com>
2026-05-23 02:43:53 +0900

    chore: add vite config for multipage build

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'auth.html'),
+        board: resolve(__dirname, 'board.html'),
+        post: resolve(__dirname, 'post.html'),
+        write: resolve(__dirname, 'write.html'),
+      },
+    },
+  },
+});
